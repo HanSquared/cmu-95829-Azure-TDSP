6994bc7a6b2bde269a96d18a2230effa4fb05565
HanSquared <HAN-SQUARE@OUTLOOK.COM>
2024-06-21 21:52:48 -0400

    delete extra codes

--- application/routes.py
+++ application/routes.py
@@ -19,30 +19,6 @@ def churnclassify():
     tenure = request.form.get("tenure")
     gender_Male = request.form.get("gender_Male")
     Dependents_Yes = request.form.get("Dependents_Yes")
-"""
-    PhoneService = request.form.get("PhoneService")
-    MultipleLines = request.form.get("MultipleLines")
-    InternetService= request.form.get("InternetService")
-    OnlineSecurity = request.form.get("OnlineSecurity")
-    OnlineBackup = request.form.get("OnlineBackup")
-    DeviceProtection = request.form.get("DeviceProtection")
-    TechSupport = request.form.get("TechSupport")
-    StreamingTV= request.form.get("StreamingTV")
-    StreamingMovies = request.form.get("StreamingMovies")
-    Contract = request.form.get("Contract")
-    PaperlessBilling = request.form.get("PaperlessBilling")
-    PaymentMethod = request.form.get("PaymentMethod")
-    MonthlyCharges = request.form.get("MonthlyCharges")
-    TotalCharges = request.form.get("TotalCharges")
-   
-   #extract data from json 
-    input_data = json.dumps({"gender":gender, "SeniorCitizen":SeniorCitizen, "Partner": Partner, "Dependents": Dependents, "tenure": tenure, \
-        "PhoneService": PhoneService, "MultipleLines": MultipleLines, "InternetService": InternetService, \
-        "OnlineSecurity": OnlineSecurity, "OnlineBackup": OnlineBackup, \
-        "DeviceProtection": DeviceProtection, "TechSupport": TechSupport, "StreamingTV": StreamingTV, \
-        "StreamingMovies": StreamingMovies, "Contract": Contract, "PaperlessBilling": PaperlessBilling, \
-        "PaymentMethod": PaymentMethod, "MonthlyCharges": MonthlyCharges, "TotalCharges": TotalCharges})
-"""
     input_data = json.dumps({"SeniorCitizen":SeniorCitizen,"tenure": tenure, "gender_Male":gender_Male, "Dependents_Yes": Dependents_Yes})
     #url for churn classification api
     url = "http://localhost:8082/api"
@@ -51,15 +27,6 @@ def churnclassify():
     #post data to url
     results =  requests.post(url, input_data)
 
-    #send input values and prediction result to index.html for display
- """   
-    return render_template("index.html", gender = gender, SeniorCitizen = SeniorCitizen, Partner = Partner, Dependents = Dependents, tenure = tenure, \
-        PhoneService = PhoneService, MultipleLines = MultipleLines, InternetService = InternetService, OnlineSecurity = OnlineSecurity, \
-        OnlineBackup = OnlineBackup, DeviceProtection = DeviceProtection, TechSupport = TechSupport, \
-        StreamingTV = StreamingTV, StreamingMovies = StreamingMovies, Contract = Contract, \
-        PaperlessBilling = PaperlessBilling, PaymentMethod = PaymentMethod, MonthlyCharges = MonthlyCharges, TotalCharges = TotalCharges, \
-        results = results.content.decode('UTF-8'))
- """  
+    #send input values and prediction result to index.html for display 
     return render_template("index.html", SeniorCitizen = SeniorCitizen,tenure = tenure, gender_Male = gender_Male, \
-        Dependents_Yes = Dependents_Yes, TotalCharges = TotalCharges, \
-        results = results.content.decode('UTF-8'))
+        Dependents_Yes = Dependents_Yes,results = results.content.decode('UTF-8'))
